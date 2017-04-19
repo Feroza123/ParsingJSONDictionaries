c508543a2760dbabb4c23ebfd0aa087e18dd47a2
Feroz Ally <feroz.ally@derivco.co.za>
2017-04-19 17:48:16 +0200

    Update Program class

--- ParsingJSONDictionaries/Program.cs
+++ ParsingJSONDictionaries/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace ParsingJSONDictionaries
 {
@@ -11,6 +13,32 @@ namespace ParsingJSONDictionaries
         //http://fixer.io/
         static void Main(string[] args)
         {
+            Currency currencyResponse = new Currency();
+            currencyResponse = Get("CAD");
+            Console.WriteLine(currencyResponse.Base);
+            Console.WriteLine(currencyResponse.Date);
+            Console.WriteLine(currencyResponse.Rates);
+        }
+
+        public static Currency Get(string type)
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    var jsonResponse = client.DownloadString("http://api.fixer.io/latest?base=" + type);
+                    Currency currencyResponse = new Currency();
+                    currencyResponse = JsonConvert.DeserializeObject<Currency>(jsonResponse);
+                    return currencyResponse;
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+
         }
     }
 }
