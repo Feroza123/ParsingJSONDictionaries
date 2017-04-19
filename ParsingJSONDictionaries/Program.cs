3c21303d8cb1f1858f997c0b838b8756b93d7453
Feroz Ally <feroz.ally@derivco.co.za>
2017-04-19 17:35:02 +0200

    Created new class for currency

---
+++ ParsingJSONDictionaries/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingJSONDictionaries
+{
+    class Program
+    {
+        //http://fixer.io/
+        static void Main(string[] args)
+        {
+        }
+    }
+}
